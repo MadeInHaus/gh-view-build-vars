5128542ee0e3a74aa65f231036c41ac76accde21
Ryan Couse <ryan@madeinhaus.com>
2020-09-09 11:09:33 -0700

    Clean up variable names + add outputs

--- main.js
+++ main.js
@@ -8,8 +8,18 @@ const project_name = core.getInput('project_name', { required: true });
 
 const branch = (context.ref && context.ref.replace('refs/heads/', '')) || 'unknown';
 const branch_slug = slugify(branch);
-
 core.debug(`branch: ${branch}`);
+core.debug(`branch_slug: ${branch_slug}`);
+core.exportVariable('HAUS_BRANCH_SLUG', branch_slug);
+core.setOutput('branch_slug', branch_slug);
 
-core.exportVariable('GITHUB_REF_SLUG_URL', branch_slug);
-core.exportVariable('PROJECT_BRANCH_SLUG', `${project_name}-${branch_slug}`);
+const project_name_slug = slugify(project_name);
+core.debug(`project_name: ${project_name}`);
+core.debug(`project_name_slug: ${project_name_slug}`);
+core.exportVariable('HAUS_PROJECT_NAME_SLUG', project_name_slug);
+core.setOutput('project_name_slug', project_name_slug);
+
+const project_name_branch_slug = `${project_name}-${branch_slug}`;
+core.debug(`project_name_branch_slug: ${project_name_branch_slug}`);
+core.exportVariable('HAUS_PROJECT_NAME_BRANCH_SLUG', project_name_branch_slug);
+core.setOutput('project_name_branch_slug', project_name_branch_slug);
